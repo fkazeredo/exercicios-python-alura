6d8dfae422b506a14809ad70468979f3b4ea6c56
Franklin Azeredo <fkazeredo.dev@gmail.com>
2020-01-02 20:05:43 -0300

    Projeto Django

---
+++ django/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/franklin/DEV/Workspace/Pessoal/exercicios-python-alura/django/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
