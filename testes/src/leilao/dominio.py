44742b1fefdaead8b41fa715a30271bc103b8eb6
Franklin Azeredo <fkazeredo.dev@gmail.com>
2020-01-02 13:19:34 -0300

    Aula de testes

--- testes/src/leilao/dominio.py
+++ testes/src/leilao/dominio.py
@@ -8,7 +8,7 @@ class Usuario:
         self.__carteira = carteira
 
     def propoe_lance(self, leilao, valor):
-        if valor > self.__carteira
+        if valor > self.__carteira:
             raise ValueError('Não pode propor um lance com valor maior que o valor da carteira')
         lance = Lance(self, valor)
         leilao.propoe(lance)
