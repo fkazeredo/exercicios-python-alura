8e18dd59f5ccbc1af5df8e875731919a9fc1cef6
Franklin Azeredo <fkazeredo.dev@gmail.com>
2020-01-03 16:50:30 -0300

    Parte 1 do curso de Django

--- django/alurareceita/settings.py
+++ django/alurareceita/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '(jb@wvw8l1wtvk97o%ya7wx(12e@9mvwss-qo90*(2-0$tz@g%'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -55,7 +53,7 @@ ROOT_URLCONF = 'alurareceita.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'receitas/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,17 +68,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'alurareceita.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_receita',
+        'USER': 'root',
+        'PASSWORD': 'ladp12',
+        'HOST': 'localhost'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -100,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -114,8 +112,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'alurareceita/static')
+]
--- django/receitas/views.py
+++ django/receitas/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Receita
 
-# Create your views here.
 def index(request):
-    return HttpResponse('<h1>Receitas</h1>')
+    receitas = Receita.objects.all()
+    dados = {
+        'receitas': receitas
+    }
+    return render(request, 'index.html', dados)
+
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+    receita_a_exibir = {
+        'receita': receita
+    }
+    return render(request, 'receita.html', receita_a_exibir)
